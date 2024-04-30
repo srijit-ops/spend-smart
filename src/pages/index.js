694e62293c448cd61299863a321a72ede2e8ebda
srijit-ops <srijitasengupta23@gmail.com>
2024-04-30 20:51:04 +0530

    landing page ui created

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,42 @@
+"use client";
+import { AuroraBackground } from "@/components/landingPageComponents/aurora-background";
+import { motion } from "framer-motion";
+import React from "react";
+
+
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
-    >
-      Hello world
+    <main>
+    <AuroraBackground>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="relative flex flex-col gap-4 items-center justify-center px-4"
+      >
+        <div className="text-2xl md:text-6xl font-bold text-white text-center tracking-wide">
+          Spend smart
+        </div>
+        <div className="font-extralight text-[0.8rem] md:text-2xl text-gray-400 py-4 tracking-wide">
+        Master Your Finances, Grow Your Wealth. Track, Save, Invest and repeat.
+        </div>
+        <div className="flex justify-between items-center flex-wrap gap-6 mt-8"> 
+        <button className="bg-transperant rounded-lg w-fit text-white px-4 py-3 border  tracking-wider border-yellow-500 hover:bg-yellow-500">
+          Browse stocks
+        </button>
+        <button className="bg-yellow-500 rounded-lg w-fit text-white tracking-wider px-4 py-3  hover:bg-yellow-600">
+          Track Expense
+        </button>
+        </div>
+      </motion.div>
+    </AuroraBackground>
+
     </main>
   );
 }
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,42 @@
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
   content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
+    // your paths
+    "./src/**/*.{js,jsx}",
   ],
-  prefix: "",
+  darkMode: "class",
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        aurora: "aurora 60s linear infinite",
+      },
+      keyframes: {
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [addVariablesForColors],
+};
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
 }
