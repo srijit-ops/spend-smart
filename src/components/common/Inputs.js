b3693526c4f6bf6e49fcfb6c155044dc80af2441
srijit-ops <srijitasengupta23@gmail.com>
2024-05-10 20:42:35 +0530

    react hook form added

---
+++ src/components/common/Inputs.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const TextInput=({label, type, placeholder, error, labelStyle, inputStyle, onChange})=> {
+  return (
+    <div className='my-4 flex flex-col'>
+        <label className='text-gray-200 tracking-wider mb-3' style={{...labelStyle}}>{label}</label>
+        <input type={type} placeholder={placeholder} onChange={onChange} className='px-4 py-3 mb-3 border border-gray-300 text-white  rounded-md bg-transparent' style={{...inputStyle}}/>
+        <p className='tracking-wider text-red-500 text-[0.9rem]'>{error}</p>
+    </div>
+  )
+}
+
+export const RadioInput= ({mainLabel, radioOptions})=>{
+    return(
+        <div className='my-4 flex flex-col'>
+        <label className='text-gray-200 tracking-wider mb-3'>{mainLabel}</label>
+        <div className='flex justify-between items-center flex-wrap'>
+        {
+                radioOptions?.map((item, index)=>{
+                    return(
+                        <div className='flex justify-start items-center gap-3' key={index}>
+                             <input type='radio' value={item.value} id={item.id}/>
+                            <label htmlFor={item.id} className='text-white'>{item.sublebel}</label>
+                        </div>
+                    )
+                })
+            }
+        </div>
+        </div>
+    )
+}
--- src/components/expenseOverviewComponents/AddExpenseModal.js
+++ src/components/expenseOverviewComponents/AddExpenseModal.js
@@ -1,14 +1,90 @@
 import React from 'react'
 import CustomModal from '../common/CustomModal'
+import { useForm, Controller } from 'react-hook-form'
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+import { RadioInput, TextInput } from '../common/Inputs'
 
 function AddExpenseModal({open, onCloseModal, title}) {
+
+  const validationSchema= yup.object({
+    expenseName: yup.string().required("Expense name is required"),
+    expenseDetail: yup.string().test({
+      name: 'max',
+      exclusive:true,
+      message: "Expense detail must not exceed 30 characters",
+      test: (value)=> value?.length <= 30
+    }),
+    amount: yup.number().min(1).required("Expense amount is required"),
+    expenseType: yup.string().required("Expense type is required")
+  })
+
+  const {handleSubmit, control, formState:{errors}}= useForm(
+    {
+    resolver: yupResolver(validationSchema),
+    default:{
+      expenseName:"",
+      expenseDetail:"",
+      amount: null,
+      expenseType:""
+    }
+    }
+  )
+  const onSubmit=(data)=>{
+    console.log(data)
+    onCloseModal()
+  }
+
+  const expenseTypeOptions=[
+    {
+      id:"debit",
+      value:"debit",
+      sublebel:"Debit"
+    },
+    {
+      id:"credit",
+      value:"credit",
+      sublebel:"Credit"
+    }
+  ]
+
   return (
     <CustomModal
         open={open}
         onCloseModal={onCloseModal}
         title={title}
       >
-        form
+        <form onSubmit={handleSubmit(onSubmit)} className='mt-7'>
+          <Controller
+          control={control}
+          name='expenseName'
+          render={({field: {onChange}})=>
+            <TextInput type={"text"} placeholder={"Expense name"} label={"Expense name"} error={errors.expenseName?.message} onChange={onChange}/>
+          }
+          />
+          <Controller
+          control={control}
+          name='expenseDetail'
+          render={({field: {onChange}})=>
+            <TextInput type={"text"} placeholder={"Expense description"} label={"Expense description"} error={errors.expenseDetail?.message} onChange={onChange}/>
+          }
+          />
+          <Controller
+          control={control}
+          name='amount'
+          render={({field: {onChange}})=>
+            <TextInput type={"number"} placeholder={"Enter the amount"} label={"Expense amount"} error={errors.amount?.message} onChange={onChange}/>
+          }
+          />
+          <Controller
+          control={control}
+          name='expenseType'
+          render={({field: {onChange}})=>
+            <RadioInput mainLabel={"Expense type"} error={errors.expenseType?.message} onChange={onChange} radioOptions={expenseTypeOptions}/>
+          }
+          />
+          <input type='submit'/>
+        </form>
       </CustomModal>
   )
 }
